5b665a172ca12c58c00c87f4486ecfb2e8b8d739
40550074 <limengbeta@163.com>
2020-01-02 16:49:00 +1300

    Add second Activity

---
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -0,0 +1,7 @@
+package com.example.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+
+}
--- CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.java
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.java
@@ -6,21 +6,10 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
